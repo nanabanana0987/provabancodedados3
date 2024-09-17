4e9e3fb994b41732654a20175b442f3641136a10
nanabanana0987 <143831995+nanabanana0987@users.noreply.github.com>
2024-09-17 17:03:42 -0300

    Add files via upload

---
+++ prova_bancodedados3bimestre.sql
@@ -0,0 +1,38 @@
+USE db_proj_exam;
+*exercicio_um*
+
+ horas_trabalhadas decimal(7) group by
+ funcionario_id int not null
+ 
+ *exercicio_dois*
+ 
+ funcionario_id int not null
+ 
+ left join
+ projeto_id int not null
+ 
+ *exercicio_tres*
+ 
+ group by
+ funcionario_id int not null
+ data_projetos varchar(20)
+ 
+ 
+ *exercicio_quatro*
+ 
+ projeto_id int not null 
+ nome varchar(20) not null
+ 
+ *exercicio_cinco*
+ 
+ funcionario_id int not null
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
